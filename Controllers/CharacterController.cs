5ab23ef2e4142c2a716593f5c2c939c8ed92a745
John Webster <jwebster@valorem.com>
2021-06-16 16:24:20 -0700

    few lessons

---
+++ Controllers/CharacterController.cs
@@ -0,0 +1,44 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using dotnet_rpg.Models;
+using dotnet_rpg.Services.CharacterService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_rpg.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CharacterController : ControllerBase
+    {
+        private static List<Character> characters = new List<Character> {
+            new Character(),
+            new Character {Id = 1, Name = "Sam"}
+        };
+        private readonly ICharacterService _characterService;
+
+        public CharacterController(ICharacterService characterService)
+        {
+            _characterService = characterService;
+
+        }
+
+        [HttpGet("GetAll")]
+        public async Task<ActionResult<ServiceResponse<List<Character>>>> Get()
+        {
+            return Ok(await _characterService.GetAllCharacters());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ServiceResponse<Character>>> GetSingle(int id)
+        {
+            return Ok(await _characterService.GetCharacterById(id));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ServiceResponse<List<Character>>>> AddCharacter(Character newCharacter)
+        {
+            return Ok(await _characterService.AddCharacter(newCharacter));
+        }
+    }
+}
---
+++ Services/CharacterService/CharacterService.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using dotnet_rpg.Models;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet_rpg.Services.CharacterService
+{
+    public class CharacterService : ICharacterService
+    {
+        private static List<Character> characters = new List<Character> {
+            new Character(),
+            new Character {Id = 1, Name = "Sam"}
+        };
+        public async Task<ServiceResponse<List<Character>>> AddCharacter(Character newCharacter)
+        {
+            var serviceResponse = new ServiceResponse<List<Character>>();
+            characters.Add(newCharacter);
+            serviceResponse.Data = characters;
+            return serviceResponse;
+        }
+
+        public async Task<ServiceResponse<List<Character>>> GetAllCharacters()
+        {
+            var serviceResponse = new ServiceResponse<List<Character>>();
+            serviceResponse.Data = characters;
+            return serviceResponse;
+        }
+
+        public async Task<ServiceResponse<Character>> GetCharacterById(int id)
+        {
+            var serviceResponse = new ServiceResponse<Character>();
+            serviceResponse.Data = characters.FirstOrDefault(c => c.Id == id);
+            return serviceResponse;
+        }
+    }
+}
